fddc3a538d6498712df707626f7543c430d7e1a4
austinl4 <wal16001@byui.edu>
2020-10-16 15:46:00 -0600

    JavaScript

---
+++ js/poundsToKilograms.js
@@ -0,0 +1,9 @@
+function poundsToKilograms() {
+    // INPUT VALUE
+    let pounds = parseFloat(document.getElementById('pounds').value);
+
+    // PROCESSING
+    let kilograms = pounds/0.45359237;
+    // OUTPUTVALUE
+    document.getElementById("output").textContent = kilograms.toFixed(1) + " Kilograms";
+}
